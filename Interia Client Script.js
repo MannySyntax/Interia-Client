6e55707cedff5e4ef0f294b26170f7223e838721
Manny <109997233+MannySyntax@users.noreply.github.com>
2023-10-20 12:30:48 -0400

    Create Interia Client Script.js

---
+++ Interia Client Script.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Battledudes.io Hack
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Join discord.gg/zeroday for more cheats!
+// @author       MannyCodes
+// @match        https://battledudes.io/
+// @icon         https://i.ibb.co/ftTpyxk/download-2.jpg
+// @grant        none
+// @require      https://gist.githubusercontent.com/MannySyntax/e28feb586a509f35eaed99c5396eb4d8/raw/ed65352a6660d1cca2be1fc1039bd8eccf9c1216/interiaClient.js
+// @run-at       document-start
+// ==/UserScript==
+
+
+// Everything is loaded from @require in order to update your hacked client easier! 🎉
+// Author: MannyCodes 👨🏻‍💻
+// Join the discord for hacks for other io games! -> discord.gg/zeroday
+// Disclaimer: This aimbot is publicised which means the features you see were rushed & the chances of this client patched is high. If you'd like me to create you your own hacked client message me on discord MannyCodes.
